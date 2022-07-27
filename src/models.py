b3e5c67b2120df063e461f3b50e3f98c5e0abb41
Samuel Palacios <samuelpl0888@gmail.com>
2022-07-27 00:01:05 +0000

    Data modeling Instagram API with SQLAlchemy

--- src/models.py
+++ src/models.py
@@ -8,26 +8,48 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(100), nullable=False)
+    first_name = Column(String(20), nullable=False)
+    last_name = Column(String(20), nullable=False)
+    email = Column(String(100), nullable=False )
+    password = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'followers'
+    followed_user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    following_user_id = Column(Integer, ForeignKey('users.id'))
+
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    created_by_user = Column(Integer, ForeignKey('users.id'))
+    caption = Column(String(150))
+    likes = Column(Integer)
+    created_time = Column(String(20), nullable=False)
 
-    def to_dict(self):
-        return {}
+class PostMedia(Base):
+    __tablename__ = 'post_media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    media_file = Column(String(100)) # should be VARBINARY
+
+
+class LikedPost(Base):
+    __tablename__ = 'liked_posts'
+    user_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'), primary_key=True)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    comment = Column(String(150))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    created_time = Column(String(20), nullable=False)
+    comment_reply_to = Column(String(150), ForeignKey('comments.id'))
 
 ## Draw from SQLAlchemy base
 try:
